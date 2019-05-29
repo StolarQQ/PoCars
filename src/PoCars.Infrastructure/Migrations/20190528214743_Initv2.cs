fbddfeedaeb70674855848e8625ef7ce0e710bea
Stolar <stolar28@gmail.com>
2019-05-29 23:20:56 +0200

    Infrastructure project with DbContext and migrations

---
+++ src/PoCars.Infrastructure/Migrations/20190528214743_Initv2.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PoCars.Infrastructure.Migrations
+{
+    public partial class Initv2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Cars",
+                type: "decimal(4,2)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Cars",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(4,2)");
+        }
+    }
+}
---
+++ src/PoCars.Infrastructure/PoCarsContext.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using PoCars.Domain;
+
+namespace PoCars.Infrastructure
+{
+    public class PoCarsContext : DbContext
+    {
+        public PoCarsContext(DbContextOptions<PoCarsContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var carBuilder = modelBuilder.Entity<Car>();
+
+            carBuilder.HasKey(x => x.Id);
+            carBuilder.Property(x => x.Id)
+                .HasAnnotation("DatabaseGenerated", DatabaseGeneratedOption.None);
+            //carBuilder.Property(x => x.Price).HasAnnotation("Column", TypeName = "decimal(4,2)");
+
+        }
+    }
+}
