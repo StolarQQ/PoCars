69a3a5662e1b09e5b1b0b70c03fc0185f6038936
Stolar <stolar28@gmail.com>
2019-05-29 23:21:34 +0200

    Viewmodel project

---
+++ src/PoCars.ViewModels/CarViewModel.cs
@@ -0,0 +1,10 @@
+﻿using PoCars.Domain;
+
+namespace PoCars.ViewModels
+{
+    public class CarViewModel
+    {
+        public string Title { get; set; }
+        public Car Car { get; set; }
+    }
+}
---
+++ src/PoCars.ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PoCars.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
