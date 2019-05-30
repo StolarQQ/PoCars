4ade1c218858b002b3e419f85d0da523c6cd74bd
Stolar <stolar28@gmail.com>
2019-05-30 23:31:33 +0200

    Added scaffolding library for Login, Register, RestorePassword pages

---
+++ src/PoCars.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using PoCars.Infrastructure;
+
+[assembly: HostingStartup(typeof(PoCars.Web.Areas.Identity.IdentityHostingStartup))]
+namespace PoCars.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<PoCarsContext>();
+            });
+        }
+    }
+}
