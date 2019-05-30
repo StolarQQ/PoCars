a4dc0e2420965f19b3a8fa0636c22758a034038c
Stolar <stolar28@gmail.com>
2019-05-30 23:29:52 +0200

    Added ViewModels

---
+++ src/PoCars.ViewModels/CarIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using PoCars.Domain;
+
+namespace PoCars.ViewModels
+{
+    public class CarIndexViewModel
+    {
+        public string Title { get; set; }
+        public IEnumerable<Car> Cars { get; set; }
+        public Car Car { get; set; }
+    }
+}
--- src/PoCars.ViewModels/CarViewModel.cs
+++ src/PoCars.ViewModels/CarViewModel.cs
@@ -1,4 +1,8 @@
-﻿using PoCars.Domain;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PoCars.Domain;
 
 namespace PoCars.ViewModels
 {
@@ -6,5 +10,37 @@ namespace PoCars.ViewModels
     {
         public string Title { get; set; }
         public Car Car { get; set; }
+
+        public List<SelectListItem> Brands { get; set; }
+        public List<SelectListItem> FuelType { get; set; }
+        public List<SelectListItem> BodyStyles { get; set; }
+
+        public CarViewModel()
+        {
+            Brands = new List<SelectListItem>();
+            BodyStyles = new List<SelectListItem>();
+            FuelType = new List<SelectListItem>();
+
+            foreach (var brand in Enum.GetValues(typeof(Brand)).Cast<Brand>())
+                Brands.Add(new SelectListItem
+                {
+                    Value = ((int) brand).ToString(),
+                    Text = brand.ToString()
+                });
+
+            foreach (var fuel in Enum.GetValues(typeof(Brand)).Cast<Fuel>())
+                FuelType.Add(new SelectListItem
+                {
+                    Value = ((int) fuel).ToString(),
+                    Text = fuel.ToString()
+                });
+
+            foreach (var bodyStyle in Enum.GetValues(typeof(BodyStyle)).Cast<BodyStyle>())
+                BodyStyles.Add(new SelectListItem
+                {
+                    Value = ((int) bodyStyle).ToString(),
+                    Text = bodyStyle.ToString()
+                });
+        }
     }
 }
